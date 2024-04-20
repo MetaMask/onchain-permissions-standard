f4841bf964f46058d197c55707e3af2c2b506763
Dan Finlay <dan@danfinlay.com>
2024-04-19 21:39:25 -0700

    add some nice intro comments

--- packages/snap/src/myLibrary.ts
+++ packages/snap/src/myLibrary.ts
@@ -1,38 +1,48 @@
 import { z } from 'zod';
 
-const PERMISSIONS_SNAP_ID = 'TODO_PLACEHOLDER';
-
-
 
 const createMyLibrary = async (options) => {
 
   const { permissions }  = options;
 
   return {
-    handler: async ({
-      origin,
-      request,
-    }) => {
 
-      // Defer to the permissions kernel for all comms.
-      if (origin !== PERMISSIONS_SNAP_ID) {
-        throw new UnauthorizedError(`Method ${request.method} not authorized for origin ${origin}.`);
-      }
+    renderAttenuatorFor: (objectId) => {
+      const permission = permissionsMap.get(objectId);
+      switch (permission.type) {
+        case 'erc20-token':
+          return panel([
+            text('What is the maximum allowance to grant?'),
+            input({
+              name: 'allowance',
+              type: 'number',
+              decimals: 18,
+              placeholder: '1',
+            }),
+            text('Expiration time?'),
+            input({
+              name: 'expiration',
+              type: 'time',
+              placeholder: Date.now() + ONE_WEEK,
+            }),
+            text('Anything else? Just for the record ;)'),
+            input({
+              name: 'llmInput',
+              type: 'text',
+              placeholder: Date.now() + ONE_WEEK,
+            }),
+          ]);
+          
+          default:
+            // This would imply a kernel error, please let us know about it:
+            throw new Error('Kernel Error: Requested attenuator not defined. Please inform MetaMask.');
+        }
+      },
 
-      // These are the internal methods that the permission system kernel requires.
-      // The permission system kernel is responsible for handling the permissions request.
-      switch (request.method) {
-        case "wallet_requestOnchainPermissions":
-          return "world!";
-    
-        case "secureMethod":
-          return "The secret is: 42";
-    
-        default:
-          throw new Error("Method not found.");
-      }
-    };
-  }
+
+
+    }
+  } 
 
 }
 
@@ -50,27 +60,7 @@ myLibrary.add([
       },
     },
     renderAttenuator: () => {
-      return panel([
-        text('What is the maximum allowance to grant?'),
-        input({
-          name: 'allowance',
-          type: 'number',
-          decimals: 18,
-          placeholder: '1',
-        }),
-        text('Expiration time?'),
-        input({
-          name: 'expiration',
-          type: 'time',
-          placeholder: Date.now() + ONE_WEEK,
-        }),
-        text('Anything else? Just for the record ;)'),
-        input({
-          name: 'llmInput',
-          type: 'text',
-          placeholder: Date.now() + ONE_WEEK,
-        }),
-      ]);
+      
     },
     grantPermission: async (recipientAddress, attenuatorResponse) => {
       const { allowance, expiration }  = attenuatorResponse;
--- packages/snap/src/sca-snap.ts
+++ packages/snap/src/sca-snap.ts
@@ -1,8 +1,31 @@
+/**
+ * This is the entrypoint for a snap that handles permissions 
+ * of some type, for some network,
+ * and wants to make those assets available to the user's discretion
+ * when connecting to websites.
+ * 
+ * The goal of this file is to handle all the ugly parts
+ * and some security critical parts
+ * of interfacing with the snaps system,
+ * so that smart contract developers
+ * can fork this repository,
+ * and may only need to edit `myLibrary.ts`.
+ * 
+ * This can allow any permission from any digital source
+ * to be gathered into the user's MetaMask inventory
+ * so those permissions can be more freely
+ * and hopefully constructively interconnected around the web.
+ * 
+ * The permission system kernel is also implemented as a snap.
+ * You can view its source code at `kernel-snap.ts`.
+ */
+
 import { OnRpcRequestHandler } from '@metamask/snaps-sdk';
 import { panel, text, form, input, button } from '@metamask/snaps-sdk';
-import { z } from 'zod';
 import { createMyLibrary } from './createMyLibrary.ts'
+const PERMISSIONS_SNAP_ID = 'TODO_PLACEHOLDER';
 
+const permissionsMap: Map<string, Permission> = new Map();
 
 type AccountSettings = {
   permissions: Array<unknown>,
@@ -12,38 +35,6 @@ const settings: AccountSettings = await getPersistedSettings() || createFreshSet
 // Attn SCA devs:
 // This is the library you implement for YOUR contract account.
 // Feel free to just modify the example library!
-const myLibrary = createMyLibrary(settings);
-
-
-// Onchain Permissions Standard interface
-const PermissionsRequest = z.object({
-  permissions: z.array(
-    z.object({
-      sessionAccount: z
-        .object({
-          caip10Address: z.string(),
-        })
-        .optional(),
-      type: z.string(),
-      data: z
-        .object({
-          caip10Address: z.string().optional(),
-          limit: z.string().optional(),
-        })
-        .optional(),
-      required: z.boolean(),
-    }),
-  ),
-});
-
-type PermissionsRequest = z.infer<typeof PermissionsRequest>;
-
-type Permission = {
-  type: string;
-  methodName: string;
-  data: Record<string, any>;
-};
-
 const myLibrary = createMyLibrary(settings);
 
 export const onRpcRequest: OnRpcRequestHandler = accountKit.handler();
@@ -57,4 +48,32 @@ function createFreshSettings () : AccountSettings {
   return {
     permissions: [],
   }
+}
+
+return {
+  handler: async ({
+    origin,
+    request,
+  }) => {
+
+    // Defer to the permissions kernel for all comms.
+    if (origin !== PERMISSIONS_SNAP_ID) {
+      throw new UnauthorizedError(`Method ${request.method} not authorized for origin ${origin}.`);
+    }
+
+    // These are the internal methods that the permission system kernel requires.
+    // The permission system kernel is responsible for handling the permissions request.
+    switch (request.method) {
+      case "accountProvider_renderAttenuatorsFor":
+        const { objectId } = request.params;
+        return myLibrary.renderAttenuatorFor(objectId);;
+  
+      case "accountProvider_grantPermission":
+        const { attenuatorResults, objectId }
+        return myLibrary.grantPermission
+  
+      default:
+        throw new Error("Method not found.");
+    }
+  };
 }
